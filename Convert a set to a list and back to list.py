c943af9533a01aa95dbc9f4dd181fa26fe2c7bd8
deanjuandcunha <132804655+deanjuandcunha@users.noreply.github.com>
2023-05-07 17:01:33 +0530

    Add files via upload

---
+++ Convert a set to a list and back to list.py
@@ -0,0 +1,13 @@
+
+my_list = [2,4,9,-10,100,1000,89,67,1,3,0,98]
+
+
+my_set = set(my_list)
+
+
+my_new_list = list(my_set)
+
+
+print("Original List:", my_list)
+print("Set:", my_set)
+print("New List:", my_new_list)
